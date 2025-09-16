b48f6df30022101bd2a598cdadd7fe8abf4e6199
Cory Wagaman-Eure <cory.wagamaneure@gmail.com>
2025-09-16 13:35:05 -0400

    Set up the functionallity for breaking the corpus into word tokens and adding the end of sentence and paragraph symbols.

--- src/cwagaman_ngram.py
+++ src/cwagaman_ngram.py
@@ -1,16 +1,40 @@
+import re
+
+
+
 corpus = input("Enter the path to your corpus: ")
 if corpus == '':
     corpus = "../data/processed/corpus_reddit.txt"
 f = open(corpus)
-lines = f.readlines()
+lines = f.read()
 
 
-ABBR = [
-    "mr", "mrs", "ms", "dr", "prof", "sr", "jr", "st", "vs",
-    "etc", "e.g", "i.e", "al", "fig", "cf",
-    "jan","feb","mar","apr","jun","jul","aug","sep","sept","oct","nov","dec",
-    "mon","tue","wed","thu","fri","sat","sun",
-    "u.s","u.k","u.n"
-]
 
-print(lines)
+def normalize(lines):
+    lines = lines.lower()
+    lines = re.sub(r'[^a-z\s]', '', lines)
+    lines = re.sub(r'\s+', ' ', lines).strip()
+    return lines
+
+def separate_sentences(text):
+    word_list = []
+    for s in re.split(r'[.!?]+', text):
+        s = s.strip()
+        word_list.append(s)
+    return word_list
+
+
+def make_tokens(text):
+    tokens = []
+    for s in separate_sentences(text):
+        s = normalize(s)
+        if not s:
+            continue
+        words = s.split()
+        tokens.append('<s>')
+        tokens.extend(words)
+        tokens.append('</s>')
+    return tokens
+
+print(make_tokens(lines))
+
